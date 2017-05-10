ade44de1c9cb9f29cc0d318ec8362e5dd0a9fce8
MauriceStrickland <maurice.strickland@viasat.com>
2017-05-09 21:19:23 -0700

    Add hashed user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = "123abc!";
 
-var decoded = jwt.verify(token, '123abc');
-console.log("Decoded: ",decoded);
+//bcrypt.genSalt(10, (err, salt) =>{
+//  bcrypt.hash(password, salt, (err, hash) => {
+//    console.log(hash);
+//  });
+//});
+
+var hashedPassword = "$2a$10$.V3RojOsyXEc5JwjpvmHWudmpH1PBmlzLQA7NbsMGHKxTkL72ePfS";
+
+bcrypt.compare('password',hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+//var data = {
+//  id: 10
+//};
+//
+//var token = jwt.sign(data, '123abc');
+//console.log(token);
+//
+//var decoded = jwt.verify(token, '123abc');
+//console.log("Decoded: ",decoded);
 
 
 
